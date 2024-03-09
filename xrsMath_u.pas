a84720ecb04e64f43f9e9ac8b109154b5d8e98ff
rocksone <141072679+rocksone@users.noreply.github.com>
2024-03-09 23:36:58 +0100

    Update xrsMath_u.pas

--- xrsMath_u.pas
+++ xrsMath_u.pas
@@ -4,14 +4,21 @@ interface
 
 // UNITS
 uses
-
-System.Variants;
+  Winapi.Windows,
+System.Variants,
+System.SysUtils,
+Vcl.Graphics;
 
 // CONST
 // RESOURCESTING
 
-// PROCEDURES
+// FUNKTIONS
  function VarToIntDef(const V: Variant; const ADefault: Integer = 0): Integer;
+ function BlackWhiteContrastColor(value: TColor):TColor;
+
+// Time
+ function Int2Time(i: Integer;  Positiv: Boolean = True; ShowZero: Boolean = False): String;
+// PROCEDURES
 
 implementation
 
@@ -27,5 +34,52 @@ begin
   else
     Result := V;
 end;
+
+function BlackWhiteContrastColor(value: TColor):TColor;
+var
+  intensity : Integer;
+begin
+  value := ColorToRGB(value);
+
+  intensity := GetBValue(value) * 21 // Blue
+    + GetGValue(value) * 174 // Green
+    + GetRValue(value) * 61; // Red
+   // intensity = 0 -> dark
+   // intensity = 255*256 -> bright
+  if intensity >= (128*256) then
+    Result := clBlack
+  else
+    Result := clWhite;
+end;
+
+
+
+function Int2Time(i: Integer;  Positiv: Boolean = True; ShowZero: Boolean = False): String;
+var
+  Stunden, RestMinuten, Hours : integer;
+begin
+  Hours := VarToIntDef(i);
+  if (Hours = 0) and not ShowZero then
+    Result := ''
+  else
+  begin
+
+    Stunden := Hours div 60;
+    RestMinuten := Hours mod 60;
+
+    if not Positiv Then
+      Result := Format('%.2d:%.2d', [Stunden, abs(RestMinuten)])
+    else
+    begin
+      if Hours > 0 then
+
+        Result := Format('+%.2d:%.2d', [Stunden, abs(RestMinuten)])
+      else
+        Result := Format('%.2d:%.2d', [Stunden, abs(RestMinuten)]);
+    end;
+  end;
+
+end;
+
 
 end.
